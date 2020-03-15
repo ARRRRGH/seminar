de83a068d28c246768d27a7f0281701d8bc0b54e
Unknown <rrrr@posteo.ch>
2020-03-15 19:35:09 +0100

    pred_array with new output

--- learn/utils.py
+++ learn/utils.py
@@ -1,5 +1,7 @@
 import xarray as xr
 import numpy as np
+import pandas as pd
+
 
 def pred_array(model, inp, arr=None, model_arr=None, batch_size=10000, no_val=-1):
     assert arr is not None or model_arr is not None
@@ -9,12 +11,20 @@ def pred_array(model, inp, arr=None, model_arr=None, batch_size=10000, no_val=-1
         shape = arr.shape
 
     # run in multiple batches for memory
+    out_df = pd.DataFrame(index=inp.index)
+    running_ind = 0
+    
     for batch in np.array_split(inp, min(batch_size, len(inp))):
-        arr[np.unravel_index(batch.index, model_arr.shape)] = model.predict(batch.values)
+        next_ind = running_ind + batch.shape[0]
+
+        out_df.iloc[running_ind:next_ind] = model.predict(batch.values)
+        arr[np.unravel_index(batch.index, model_arr.shape)] = out_df.iloc[running_ind:next_ind].values
+
+        running_ind = next_ind
 
     arr = xr.DataArray(arr, dims=['y', 'x'])
 
     arr.attrs = model_arr.attrs.copy()
     arr = arr.assign_coords(model_arr.coords)
 
-    return arr
+    return arr, out_df
