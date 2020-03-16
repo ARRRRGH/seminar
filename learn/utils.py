899802433f4dfdfffef0da11335536f6f01b79da
Unknown <rrrr@posteo.ch>
2020-03-16 10:03:25 +0100

    make multithreaded pred_array

--- learn/utils.py
+++ learn/utils.py
@@ -1,22 +1,42 @@
 import xarray as xr
 import numpy as np
 import pandas as pd
+from joblib import Parallel, delayed
+from functools import partial
 
 
-def pred_array(model, inp, arr=None, model_arr=None, batch_size=10000, no_val=-1):
+def run_jobs(jobs, joblib=True, n_jobs=4):
+    if joblib:
+        jobs = [delayed(job)() for job in jobs]
+        out = Parallel(n_jobs=n_jobs, backend='threading')(jobs)
+    else:
+        out = []
+        for job in jobs:
+            out.append(job())
+    return out
+
+
+def pred_array(model, inp, arr=None, model_arr=None, batch_size=10000, no_val=-1, n_jobs=6):
     assert arr is not None or model_arr is not None
     if arr is None:
         arr = np.ones(model_arr.shape) * no_val
+        shape = model_arr.shape
     if model_arr is None:
         shape = arr.shape
 
     # run in multiple batches for memory
-    out_df = pd.DataFrame(index=inp.index)
-    
+    jobs = []
+    inds = []
     for batch in np.array_split(inp, min(batch_size, len(inp))):
-        tmp =  model.predict(batch.values)
-        out_df.loc[batch.index, :] =  tmp
-        arr[np.unravel_index(batch.index, model_arr.shape)] = tmp
+        jobs.append(partial(model.predict, batch.values))
+        inds.append(batch.index)
+
+    out = run_jobs(jobs, n_jobs=n_jobs)
+
+    out_df = pd.DataFrame(index=inp.index)
+    for i, pred in zip(inds, out):
+        out_df.loc[i, :] =  pred
+        arr[np.unravel_index(i, shape)] = pred
 
     arr = xr.DataArray(arr, dims=['y', 'x'])
 
