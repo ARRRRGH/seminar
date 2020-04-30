5d2a9c8ec83d6bdb57eede7e8eb2d7f82ac845d1
jim <rrrr@posteo.ch>
2020-04-30 11:23:00 +0200

    add run_jobs from dartinv

--- utils.py
+++ utils.py
@@ -1,16 +1,50 @@
 from joblib import Parallel, delayed
 
+def run_jobs(jobs, joblib=True, n_jobs=4, chunks=1, chunk_callback=None, *args, **kwargs):
+    if len(jobs) == 0:
+        return None, None
 
-def run_jobs(jobs, joblib=True, n_jobs=4):
     if joblib:
         jobs = [delayed(job)() for job in jobs]
-        out = Parallel(n_jobs=n_jobs, backend='threading')(jobs)
+
+        chunk_size = max(1, len(jobs) // chunks)
+        chunks = [jobs[i:i + chunk_size] for i in range(0, len(jobs), chunk_size)]
+
+        out = []
+        for chunk in chunks:
+            chunk_out = Parallel(n_jobs=n_jobs, *args, **kwargs)(chunk)
+            if chunk_callback is not None:
+                ret = chunk_callback(chunk_out, args=[job[0].args for job in chunk],
+                                     kwargs=[job[0].keywords for job in chunk])
+                out.append((chunk_out, ret))
+            else:
+                out.append((chunk_out, ))
     else:
         out = []
-        for job in jobs:
-            out.append(job())
-    return out
+        # create chunks
+        nr_chunks = chunks
+        chunk_size = max(1, len(jobs) // chunks)
+        chunks = [jobs[i:i + chunk_size] for i in range(0, len(jobs), chunk_size)]
 
+        for j, chunk in enumerate(chunks):
+            chunk_out = []
+
+            for i, job in enumerate(chunk):
+                if 'verbose' in kwargs and kwargs['verbose']:
+                    print('\r\r Chunk %d / %d' % (j, nr_chunks) +
+                          '\n Working on job %d/%d, ' % (i, len(chunk)) +
+                          '\n args: %s, \n kwargs: %s' % (', '.join(job.args), ', '.join([str(tup)
+                                                                                     for tup in job.keywords.items()])))
+                chunk_out.append(job())
+
+            if chunk_callback is not None:
+                ret = chunk_callback(chunk_out, args=[job.args for job in chunk],
+                                     kwargs=[job.keywords for job in chunk])
+                out.append((chunk_out, ret))
+            else:
+                out.append((chunk_out, ))
+
+    return list(el[0] for el in zip(*out))
 
 class BiDict(dict):
     def __init__(self, *args, **kwargs):
